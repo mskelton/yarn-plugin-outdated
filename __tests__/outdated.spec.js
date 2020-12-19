f247b53bd48d74e8f2dd162dc20890767af8f40d
Mark Skelton <mdskelton99@gmail.com>
2020-12-18 23:32:28 -0600

    Add scope test

--- __tests__/outdated.spec.js
+++ __tests__/outdated.spec.js
@@ -2,6 +2,7 @@ import { formatUtils } from "../__mocks__/@yarnpkg/core"
 import { run } from "./utils"
 
 const candidates = {
+  "@test-scope/package-a": "^3.4.0",
   "package-a": "1.1.0",
   "package-b": "^1.3.0",
   "package-c": "~1.2.1",
@@ -79,6 +80,22 @@ describe("yarn outdated", () => {
           "package-b": "^1.1.1",
           "package-c": "next",
           "package-d": "workspace:^1.2.3",
+        },
+      },
+    })
+
+    expect(output).toMatchSnapshot()
+  })
+
+  it("should properly format scoped dependencies", async () => {
+    const output = await run("outdated", {
+      candidates,
+      manifest: {
+        dependencies: {
+          "@test-scope/package-a": "^3.0.1",
+        },
+        devDependencies: {
+          "package-a": "^1.0.1",
         },
       },
     })
