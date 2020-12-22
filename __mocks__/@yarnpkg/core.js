4169f06552ebdbea8087b5685cdbafa601b9c879
Mark Skelton <mskelton@widen.com>
2020-12-22 15:34:49 -0600

    Add patterns spec

--- __mocks__/@yarnpkg/core.js
+++ __mocks__/@yarnpkg/core.js
@@ -1,9 +1,16 @@
 export const Cache = { find: jest.fn() }
 export const Configuration = { find: jest.fn() }
 export const Project = { find: jest.fn() }
+
 export const structUtils = {
   stringifyIdent: ({ name, scope }) => (scope ? `@${scope}/${name}` : name),
 }
+
 export const formatUtils = {
   pretty: jest.fn(),
+  prettyList: jest.fn(),
+}
+
+export const FormatType = {
+  CODE: "CODE",
 }
