557a3bd371b8a1fefa5d6ab219bb8750c75e0fe9
Mark Skelton <mdskelton99@gmail.com>
2021-07-03 16:01:39 -0500

    Stream report (#12)
    * Update @babel/core to ^7.14.6 (and 5 others)
    
    Commit generated via `yarn stage`
    
    * Remove plugin
    
    * Updated UI to use stream report for better progress
    
    * Update bundle
    
    * Add stream report mock
    
    * Update snaps
    
    * Update bundle
    
    * Fix snap
    
    * Make success prettier

--- __mocks__/@yarnpkg/core.js
+++ __mocks__/@yarnpkg/core.js
@@ -14,3 +14,22 @@ export const formatUtils = {
 export const FormatType = {
   CODE: "CODE",
 }
+
+export const MessageName = {
+  UNNAMED: "UNNAMED",
+}
+
+export const StreamReport = {
+  progressViaCounter() {},
+  async start({ stdout }, cb) {
+    await cb({
+      reportInfo: (_, msg) => stdout.write(msg + "\n"),
+      reportProgress: () => stdout.write("--- PROGRESS ---\n"),
+      reportSeparator: () => stdout.write("--- SEPARATOR ---\n"),
+      async startTimerPromise(msg, cb) {
+        stdout.write(msg + "\n")
+        await cb()
+      },
+    })
+  },
+}
