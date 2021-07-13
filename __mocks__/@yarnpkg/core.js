0207c211fe19fa8dda710e006751008d7607d326
Mark Skelton <mskelton@widen.com>
2021-07-13 10:08:19 -0500

    Update snaps

--- __mocks__/@yarnpkg/core.js
+++ __mocks__/@yarnpkg/core.js
@@ -29,9 +29,10 @@ export const StreamReport = {
         stdout.write(`ERROR: ${msg}\n`)
         exitCode = 1
       },
-      reportInfo: (_, msg) => stdout.write(msg + "\n"),
+      reportInfo: (_, msg) => stdout.write(`INFO: ${msg}\n`),
       reportProgress: () => stdout.write("--- PROGRESS ---\n"),
       reportSeparator: () => stdout.write("--- SEPARATOR ---\n"),
+      reportWarning: (_, msg) => stdout.write(`WARN: ${msg}\n`),
       async startTimerPromise(msg, cb) {
         stdout.write(msg + "\n")
         await cb()
