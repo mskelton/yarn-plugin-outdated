8f8aae612510e7d3a7e55f4d0356c29801b406e0
Mark Skelton <mdskelton99@gmail.com>
2021-08-29 09:31:54 -0500

    Move specs

--- test/specs/outdated.spec.ts
+++ test/specs/outdated.spec.ts
@@ -79,4 +79,33 @@ test.describe("yarn outdated", () => {
     expect(stderr).toBe("")
     expect(code).toBe(1)
   })
+
+  test("defers to lockfile over manifest to check if a package is outdated", async ({
+    env,
+  }) => {
+    const { run, writeJSON } = env
+
+    await writeJSON("package.json", { dependencies: { patch: "^1.0.0" } })
+    await run("install")
+
+    const { stderr, stdout } = await run("outdated")
+    expect(stdout).toMatchSnapshot("lockfile.txt")
+    expect(stderr).toBe("")
+  })
+
+  test("respects resolutions to determine if a package is outdated", async ({
+    env,
+  }) => {
+    const { run, writeJSON } = env
+
+    await writeJSON("package.json", {
+      dependencies: { minor: "1.0.0" },
+      resolutions: { minor: "1.0.1" },
+    })
+    await run("install")
+
+    const { stderr, stdout } = await run("outdated")
+    expect(stdout).toMatchSnapshot("resolutions.txt")
+    expect(stderr).toBe("")
+  })
 })
