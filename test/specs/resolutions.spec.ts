f3b73c5e5b55433eb683b933b9d02042c81cf3fe
Mark Skelton <mdskelton99@gmail.com>
2021-08-29 07:24:54 -0500

    Add resolutions test

---
+++ test/specs/resolutions.spec.ts
@@ -0,0 +1,19 @@
+import { expect, test } from "../fixtures/env"
+
+test.describe("Resolutions", () => {
+  test("respects resolutions to determine if a package is outdated", async ({
+    env,
+  }) => {
+    const { run, writeJSON } = env
+
+    await writeJSON("package.json", {
+      dependencies: { patch: "1.0.0" },
+      resolutions: { patch: "1.1.0" },
+    })
+    await run("install")
+
+    const { stderr, stdout } = await run("outdated")
+    expect(stdout).toMatchSnapshot("resolutions.txt")
+    expect(stderr).toBe("")
+  })
+})
