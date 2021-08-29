5409f08187cad6d53c2e8964a8645d8126ed71d5
Mark Skelton <mdskelton99@gmail.com>
2021-08-29 07:35:13 -0500

    Update resolution test

--- test/specs/resolutions.spec.ts
+++ test/specs/resolutions.spec.ts
@@ -1,18 +1,17 @@
 import { expect, test } from "../fixtures/env"
 
-test.describe("Resolutions", () => {
-  test("respects resolutions to determine if a package is outdated", async ({
-    env,
-  }) => {
-    const { run, writeJSON } = env
+test("respects resolutions to determine if a package is outdated", async ({
+  env,
+}) => {
+  const { run, writeJSON } = env
 
-    await writeJSON("package.json", { workspaces: ["a"] })
-    await writeJSON("a/package.json", { dependencies: { patch: "1.0.0" } })
-    await run("set resolution patch 1.1.0")
-    await run("install")
-
-    const { stderr, stdout } = await run("outdated")
-    expect(stdout).toMatchSnapshot("resolutions.txt")
-    expect(stderr).toBe("")
+  await writeJSON("package.json", {
+    dependencies: { minor: "1.0.0" },
+    resolutions: { minor: "1.0.1" },
   })
+  await run("install")
+
+  const { stderr, stdout } = await run("outdated")
+  expect(stdout).toMatchSnapshot("resolutions.txt")
+  expect(stderr).toBe("")
 })
