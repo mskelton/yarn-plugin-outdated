eaab4bc3887b84cc84ea2d13c3c6a27a564a837f
Mark Skelton <mdskelton99@gmail.com>
2022-02-26 16:13:41 -0600

    Update tests

--- test/specs/all.spec.ts
+++ test/specs/all.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "../fixtures/env"
 
-test.describe.parallel("yarn outdated --all", () => {
+test.describe.parallel("workspaces", () => {
   test("includes packages from all workspaces", async ({ env }) => {
     const { run, writeJSON } = env
 
@@ -9,7 +9,7 @@ test.describe.parallel("yarn outdated --all", () => {
     await writeJSON("b/package.json", { dependencies: { minor: "1.0.0" } })
     await run("install")
 
-    const { stderr, stdout } = await run("outdated --all")
+    const { stderr, stdout } = await run("outdated")
     expect(stdout).toMatchSnapshot("all-workspaces.txt")
     expect(stderr).toBe("")
   })
@@ -17,10 +17,14 @@ test.describe.parallel("yarn outdated --all", () => {
   test("falls back to the computed workspace name", async ({ env }) => {
     const { run, writeJSON } = env
 
-    await writeJSON("package.json", { dependencies: { patch: "1.0.0" } })
+    await writeJSON("package.json", {
+      dependencies: { patch: "1.0.0" },
+      workspaces: ["a"],
+    })
+    await writeJSON("a/package.json", { dependencies: { patch: "1.0.1" } })
     await run("install")
 
-    const { stderr, stdout } = await run("outdated -a")
+    const { stderr, stdout } = await run("outdated")
     expect(stdout).toMatchSnapshot("computed-workspace-name.txt")
     expect(stderr).toBe("")
   })
@@ -40,8 +44,23 @@ test.describe.parallel("yarn outdated --all", () => {
     })
     await run("install")
 
-    const { stderr, stdout } = await run("outdated --all")
+    const { stderr, stdout } = await run("outdated")
     expect(stdout).toMatchSnapshot("private.txt")
     expect(stderr).toBe("")
+  })
+
+  test.describe("--workspace", () => {
+    test("only includes packages in the current workspace", async ({ env }) => {
+      const { run, writeJSON } = env
+
+      await writeJSON("package.json", { workspaces: ["a", "b"] })
+      await writeJSON("a/package.json", { dependencies: { patch: "1.0.0" } })
+      await writeJSON("b/package.json", { dependencies: { minor: "1.0.0" } })
+      await run("install")
+
+      const { stderr, stdout } = await run("outdated --workspace")
+      expect(stdout).toMatchSnapshot("current-workspace.txt")
+      expect(stderr).toBe("")
+    })
   })
 })
