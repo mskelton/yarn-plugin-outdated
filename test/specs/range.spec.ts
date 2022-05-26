3ac50519c6e736bad96f68a2e1f4c4da59b90727
Mark Skelton <mdskelton99@gmail.com>
2022-05-25 22:28:50 -0500

    Add tests

---
+++ test/specs/range.spec.ts
@@ -0,0 +1,26 @@
+import { expect, test } from "../fixtures/env"
+
+test.describe.parallel("yarn outdated --range", () => {
+  test("includes the latest version satisfying the range", async ({
+    run,
+    writeJSON,
+  }) => {
+    // Lock the versions to 1.0.0 before adding the range specifiers
+    await writeJSON("package.json", {
+      dependencies: { major: "1.0.0", minor: "1.0.0", patch: "1.0.0" },
+    })
+    await run("install")
+
+    // Add the range specifiers and run an immutable install to prevent
+    // modifying the locked versions
+    await writeJSON("package.json", {
+      dependencies: { major: "^1.0.0", minor: "~1.0.0", patch: "1.0.0" },
+    })
+    await run("install --immutable")
+
+    const { code, stderr, stdout } = await run("outdated --range")
+    expect(stdout).toMatchSnapshot("range.txt")
+    expect(stderr).toBe("")
+    expect(code).toBe(0)
+  })
+})
